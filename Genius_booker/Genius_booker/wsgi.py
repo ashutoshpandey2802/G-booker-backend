0a6428647de402ba0a3eea3af4eaee5f808b080a
ashutoshpandey2802 <ashutosh.p.2802@gmail.com>
2024-09-20 14:46:09 +0530

    wsgi update

--- Genius_booker/Genius_booker/wsgi.py
+++ Genius_booker/Genius_booker/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for Genius_booker project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
+import sys
 import os
 
-from django.core.wsgi import get_wsgi_application
-
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Genius_booker.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
