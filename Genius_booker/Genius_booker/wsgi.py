6cd53bde20bace5fd84fb9fb20452cbe1b18e367
ashutoshpandey2802 <ashutosh.p.2802@gmail.com>
2024-09-20 19:16:38 +0530

    wsgi

--- Genius_booker/Genius_booker/wsgi.py
+++ Genius_booker/Genius_booker/wsgi.py
@@ -1,8 +1,6 @@
-import sys
 import os
+from django.core.wsgi import get_wsgi_application
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Genius_booker.settings')
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
